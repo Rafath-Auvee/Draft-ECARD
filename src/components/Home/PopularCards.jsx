3eb4f5671f52fa6a358e36efa7637e42124dab3d
Rafath-Auvee <rbza98@outlook.com>
2023-06-02 12:12:00 +0600

    Navbar Fixed

--- src/components/Home/PopularCards.jsx
+++ src/components/Home/PopularCards.jsx
@@ -1,27 +1,29 @@
 "use client";
 import React from "react";
-
 import { useState } from "react";
 import { data } from "../../Data/Card_Data";
 import Cards from "@/components/Cards/Cards";
 
 const PopularCards = () => {
-
-  const cardLimit = 8; // Set the limit to 8 cards
-  let cardCount = 0; // Counter variable
-
+  const cardLimit = 8;
+  let cardCount = 0;
 
   const [activeTab, setActiveTab] = useState("singlePage");
   const handleTabClick = (tab) => {
     setActiveTab(tab);
   };
+
   return (
     <div>
       <div className="flex flex-col justify-between ">
         <div className="flex flex-col items-center justify-center h-full mt-[6rem]">
+
+
           <h1 className="font-bold text-[56px] text-primary capitalize text-center">
             Our Popular Cards
           </h1>
+          
+          
           <div className="tabs mt-[32px] mb-[58px] leading-6">
             <a
               className={`tab ${
@@ -48,10 +50,12 @@ const PopularCards = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-20  gap-4 md:mx-10 lg:mx-24 ">
           {data.map((items, index) => {
-            
+
+
             if (cardCount >= cardLimit) {
-              return null; 
+              return null;
             }
+
 
             if (
               activeTab === "singlePage" &&
@@ -66,6 +70,8 @@ const PopularCards = () => {
               cardCount++;
               return <Cards key={index} items={items} />;
             }
+
+
             return null;
           })}
         </div>
