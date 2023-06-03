b3d91c77bdc6e63023146f6c881c5e1f5fc0f325
Rafath-Auvee <rbza98@outlook.com>
2023-06-03 12:12:00 +0600

    Draft Removed

--- src/app/draft/page.jsx
+++ src/app/draft/page.jsx
@@ -9,37 +9,7 @@ import Navbar from "@/components/Navbar/Navbar";
 export default function Home() {
   return (
     <div>
-      <div
-        className="w-full h-screen bg-cover bg-no-repeat bg-scroll"
-        style={{ backgroundImage: `url('/Home/hero.png')` }}
-      >
-        <Navbar />
-        <div className="flex flex-col items-center justify-center h-screen">
-          <div className="flex flex-col items-center justify-center h-full">
-            <p className="text-[24px] text-primary uppercase">Looking for</p>
-            <h1 className="font-bold text-center text-[56px] text-primary capitalize">
-              the Perfect E-card?
-            </h1>
-            <div className="flex flex-row items-center justify-center">
-              <button className="bg-primary text-white py-[1rem] text-[18px] px-[1.5rem] mt-[24px] rounded flex items-center justify-center">
-                Check out The Designs Here
-                <BsArrowRight className="text-[20px] ml-[10px] mt-[2px]" />
-              </button>
-            </div>
-          </div>
-          <div className="flex items-center justify-center flex-grow mb-[64px] md:mb-[137px] lg:mb-[]">
-            <Image
-              src={Iphone}
-              width={0}
-              height={0}
-              className=""
-              alt="Hero Image"
-            />
-          </div>
-        </div>
-      </div>
-
-      <Footer />
+      
     </div>
   );
 }
