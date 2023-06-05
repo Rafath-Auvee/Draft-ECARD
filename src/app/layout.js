c0e68f9afc6aed47aa4377cf145bc4f4932d0a8f
Rafath-Auvee <rbza98@outlook.com>
2023-06-05 12:12:00 +0600

    Comment Removed in Layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,5 @@
 import "./globals.css";
 import { Fraunces } from "next/font/google";
-import localFont from "@next/font/local";
-
-// const satoshi = localFont({
-//   src: "../../public/fonts/Satoshi-Variable.ttf", // Adjust the path based on the actual location
-//   variable: "--font-satoshi",
-// });
-
 const fraunces = Fraunces({ subsets: ["latin"], variable: "--font-fraunces" });
 
 export const metadata = {
