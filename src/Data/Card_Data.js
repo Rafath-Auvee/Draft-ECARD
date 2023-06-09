6716dfa995eb5dc74ff68f03bca43bab27eb518b
Rafath-Auvee <rbza98@outlook.com>
2023-06-09 12:12:00 +0600

    Data.json added

--- src/Data/Card_Data.js
+++ src/Data/Card_Data.js
@@ -202,6 +202,34 @@ export const data = [
       buttonText: "View Design",
       cardType: "Multiple Page"
     },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
 
 
   ];
