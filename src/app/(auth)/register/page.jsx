ad752fe1be45bd4f5ca4f1d553b2c7624ded1954
Rafath-Auvee <rbza98@outlook.com>
2023-06-14 18:49:58 +0600

    first commit

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,109 @@
+import Image from "next/image";
+import React from "react";
+import HeroImage from "/public/images/hero_login.png";
+import Facebook from "/public/icons/facebook.svg";
+import Google from "/public/icons/google.svg";
+import IconRight from "/public/icons/Icon-right.svg";
+
+const Register = () => {
+  return (
+    <div>
+      <div className="min-h-screen bg-base-100">
+        <div className="flex flex-row">
+          <div className="basis-1/2">
+            <Image
+              src={HeroImage}
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "auto", height: "100%" }}
+              alt="Login Image"
+            />
+          </div>
+          <div className="basis-1/2 ">
+            <div className="grid place-items-center h-screen">
+              <div className="join join-vertical">
+                <div className="join-item">
+                  <h1 className="text-primary text-4xl text-center mb-12">
+                    Welcome to Save the Date
+                  </h1>
+                  <div className="flex flex-col ">
+                    <div className="btn btn-outline text-[1.2rem] w-[30rem] h-[3.5rem] mb-4">
+                      <Google />
+                      <button className="font-medium">
+                        Sign Up with Google
+                      </button>
+                    </div>
+                    <div className="btn btn-outline text-[1.2rem] w-[30rem] h-[3.5rem] mb-7">
+                      <Facebook />
+                      <button className="font-medium">
+                        Sign Up with Facebook
+                      </button>
+                    </div>
+                  </div>
+                  <div className="divider divide-[#E5E7EB] mb-10"></div>
+                  <div className="form-control  join-item text-base">
+                    <div className="mb-7">
+                      <label className="label">
+                        <span className="text-xl">Username or Email *</span>
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="Type here"
+                        className="input input-bordered w-full "
+                      />
+                      {/* <label className="label">
+                        <span className="label-text-alt text-error text-base">
+                          Email Error Message
+                        </span>
+                      </label> */}
+                    </div>
+                    <div>
+                      <label className="label">
+                        <span className="text-xl">Password *</span>
+                      </label>
+                      <input
+                        type="password"
+                        placeholder="Type here"
+                        className="input input-bordered w-full "
+                      />
+                      {/* <label className="label">
+                        <span className="text-base text-error">
+                          Password Error Message
+                        </span>
+                      </label> */}
+                    </div>
+                    <div className="form-control mb-5 mt-4 ">
+                      <div>
+                        <label className="flex items-center cursor-pointer">
+                          <input
+                            type="checkbox"
+                            // checked={false}
+                            className="checkbox"
+                          />
+                          <span className="text-xl ml-5">Remember me</span>
+                        </label>
+                      </div>
+                    </div>
+                    <button className="btn btn-primary mb-8 text-white text-[20px] capitalize font-medium py-4 pb-[36px]">
+                      Sign Up
+                    </button>
+                    <div className="flex flex-row items-center justify-between">
+                      <h3 className="text-xl"> Don’t have an account?</h3>
+                      <button className="border-2 border-[#DADDE7] text-[#23272A] pl-16 pr-16 py-3 rounded-2xl flex flex-row items-center justify-center text-2xl">
+                        <span className="mr-3">Sign Up</span>
+                        <IconRight />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        sans: ["var(--font-satoshi)", ...fontFamily.sans],
+        mono: ["var(--font-fraunces)", ...fontFamily.mono],
+      },
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#23272A", // Foundation /Blue/blue-500
+          secondary: "#4F5255",
+          accent: "#E4DE4B", // main button -> Check out the Designs here
+          neutral: "#333c4d",
+          "base-100": "#ffffff",
+          info: "#D5B048", // menu under line color
+          success: "#36d399",
+          warning: "#fbbd23",
+          "Download-Button-100": "#D5B048",
+          error: "#EF4444", // error
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
