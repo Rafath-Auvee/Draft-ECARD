6986c1b6f67a2c0451cd3f90d6371ab9d18f5cb9
Rafath-Auvee <rbza98@outlook.com>
2023-06-18 16:34:10 +0600

    Home Page 2 Sections Done

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,64 @@
+"use client";
+
 import Image from "next/image";
 import Card from "../../components/Home/Card";
 import Card_Preview from "/public/Home/card_preview.png";
 import Hero from "/public/Home/hero.png";
 import Iphone from "/public/Home/iPhone.png";
 import { BsArrowRight } from "react-icons/bs";
+import { useState } from "react";
+
+export const data = [
+  {
+    imageUrl: "/Home/card_preview.png",
+    title: "Marble Textured Welcome Board",
+    price: "৳2,500",
+    buttonText: "View Design",
+  },
+  {
+    imageUrl: "/Home/card_preview.png",
+    title: "Marble Textured Welcome Board",
+    price: "৳2,500",
+    buttonText: "View Design",
+  },
+  {
+    imageUrl: "/Home/card_preview.png",
+    title: "Marble Textured Welcome Board",
+    price: "৳2,500",
+    buttonText: "View Design",
+  },
+  {
+    imageUrl: "/Home/card_preview.png",
+    title: "Marble Textured Welcome Board",
+    price: "৳2,500",
+    buttonText: "View Design",
+  },
+  {
+    imageUrl: "/Home/card_preview.png",
+    title: "Marble Textured Welcome Board",
+    price: "৳2,500",
+    buttonText: "View Design",
+  },
+  {
+    imageUrl: "/Home/card_preview.png",
+    title: "Marble Textured Welcome Board",
+    price: "৳2,500",
+    buttonText: "View Design",
+  },
+  {
+    imageUrl: "/Home/card_preview.png",
+    title: "Marble Textured Welcome Board",
+    price: "৳2,500",
+    buttonText: "View Design",
+  },
+  // Add more data objects as needed
+];
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState("singlePage");
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
   return (
     <div>
       <div
@@ -15,11 +68,11 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center h-screen">
           <div className="flex flex-col items-center justify-center h-full mt-[6rem]">
             <p className="text-[24px] text-primary uppercase">Looking for</p>
-            <h1 className="font-bold text-[56px] text-primary capitalize">
+            <h1 className="font-bold text-center text-[56px] text-primary capitalize">
               the Perfect E-card?
             </h1>
             <div className="flex flex-row items-center justify-center">
-              <button className="bg-primary text-white py-[1rem] px-[1.5rem] mt-[24px] rounded flex items-center justify-center">
+              <button className="bg-primary text-white py-[1rem] text-[18px] px-[1.5rem] mt-[24px] rounded flex items-center justify-center">
                 Check out The Designs Here
                 <BsArrowRight className="text-[20px] ml-[10px] mt-[2px]" />
               </button>
@@ -35,86 +88,60 @@ export default function Home() {
             />
           </div>
         </div>
-
-        {/* <div className="flex items-end justify-center h-screen">
-          <Image
-            src={Iphone}
-            width={0}
-            height={0}
-            className=""
-            alt="Hero Image"
-          />
-        </div> */}
       </div>
 
-      <div className="flex flex-row justify-between ">
-        <div className="main-card basis-3/12 mx-10 my-4 bg-[#E1EDFA] px-8 py-3">
-          <div className="h-full w-full items-center ">
-            <p className="bg-[#4b52583e] text-base-100 w-36 mb-4 h-11 text-center items-center flex rounded-full justify-center">
-              Single page
-            </p>
-            <div>
-              <Image
-                src={Card_Preview}
-                alt="Card-Preview"
-                className="rounded-xl"
-              />
-            </div>
-            <h2 className="font-bold text-lg">Marble Textured Welcome Board</h2>
-            <p className="font-bold text-lg">৳2,500</p>
-            <button>View Design</button>
+      <div className="flex flex-col justify-between ">
+        <div className="flex flex-col items-center justify-center h-full mt-[6rem]">
+          <h1 className="font-bold text-[56px] text-primary capitalize text-center">
+            Our Popular Cards
+          </h1>
+          <div className="tabs mt-[32px]  mb-[58px] leading-6">
+            <a
+              className={`tab ${
+                activeTab === "singlePage"
+                  ? "tab-active underline underline-offset-8 "
+                  : ""
+              } text-[16px]`}
+              onClick={() => handleTabClick("singlePage")}
+            >
+              Single Page Cards
+            </a>
+            <a
+              className={`tab ${
+                activeTab === "multiplePage"
+                  ? "tab-active underline underline-offset-8 "
+                  : ""
+              } text-[16px]`}
+              onClick={() => handleTabClick("multiplePage")}
+            >
+              Multiple Page Cards
+            </a>
           </div>
         </div>
-        <div className="main-card basis-3/12 mx-10 my-4 bg-[#B5F2E3] px-8 py-3">
-          <div className="h-full w-full items-center ">
-            <p className="bg-[#4b52583e] text-base-100 w-36 mb-4 h-11 text-center items-center flex rounded-full justify-center">
-              Single page
-            </p>
-            <div>
-              <Image
-                src={Card_Preview}
-                alt="Card-Preview"
-                className="rounded-xl"
-              />
+
+        {/* <div className="grid grid-rows-6 md:grid-rows-4 lg:grid-rows-2 grid-flow-col gap-4 md:mx-10 gap-y-10"> */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-20  gap-4 md:mx-10 lg:mx-24 ">
+          {data.map((item, index) => (
+            <div className="mx-5  lg:mx-10 " key={index}>
+              <div
+                className="w-full h-[30rem] md:h-[25rem]  lg:h-[20rem] items-center bg-contain bg-no-repeat"
+                style={{ backgroundImage: `url('${item.imageUrl}')` }}
+              >
+                <div className="bg-[#23272a93] relative z-0 w-32 mb-4 ml-4 h-11 text-center items-center flex rounded-sm justify-center ">
+                  <p className="text-base-100">Single page</p>
+                </div>
+              </div>
+              <div className="flex flex-row mt-4 justify-between">
+                <h2 className="font-normal text-[1.5rem] md:text-[1.4rem] lg:text-xl basis-2/3">
+                  {item.title}
+                </h2>
+                <p className="font-bold text-2xl lg:text-xl basis-1/3 text-right md:text-left lg:text-right">
+                  {item.price}
+                </p>
+              </div>
+              
             </div>
-            <h2 className="font-bold text-lg">Marble Textured Welcome Board</h2>
-            <p className="font-bold text-lg">৳2,500</p>
-            <button>View Design</button>
-          </div>
-        </div>
-        <div className="main-card basis-3/12 mx-10 my-4 bg-[#DEF2B5] px-8 py-3">
-          <div className="h-full w-full items-center ">
-            <p className="bg-[#4b52583e] text-base-100 w-36 mb-4 h-11 text-center items-center flex rounded-full justify-center">
-              Single page
-            </p>
-            <div>
-              <Image
-                src={Card_Preview}
-                alt="Card-Preview"
-                className="rounded-xl"
-              />
-            </div>
-            <h2 className="font-bold text-lg">Marble Textured Welcome Board</h2>
-            <p className="font-bold text-lg">৳2,500</p>
-            <button>View Design</button>
-          </div>
-        </div>
-        <div className="main-card basis-3/12 mx-10 my-4 bg-[#C9B5F2] px-8 py-3">
-          <div className="h-full w-full items-center ">
-            <p className="bg-[#4b52583e] text-base-100 w-36 mb-4 h-11 text-center items-center flex rounded-full justify-center">
-              Single page
-            </p>
-            <div>
-              <Image
-                src={Card_Preview}
-                alt="Card-Preview"
-                className="rounded-xl"
-              />
-            </div>
-            <h2 className="font-bold text-lg">Marble Textured Welcome Board</h2>
-            <p className="font-bold text-lg">৳2,500</p>
-            <button>View Design</button>
-          </div>
+          ))}
         </div>
       </div>
     </div>
