0db79a0f27334d4df3aad66a6abbca5619aa0607
Rafath-Auvee <rbza98@outlook.com>
2023-06-19 12:37:59 +0600

    Data.json data added

--- src/Data/Card_Data.js
+++ src/Data/Card_Data.js
@@ -90,6 +90,118 @@ export const data = [
       buttonText: "View Design",
       cardType: "Multiple Page"
     },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
+    {
+      imageUrl: "/Home/card_preview.png",
+      title: "Marble Textured Welcome Board",
+      price: "৳2,500",
+      buttonText: "View Design",
+      cardType: "Multiple Page"
+    },
 
 
   ];
